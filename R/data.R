f53f110c13bd5b55104306e5df19bcd0951331d8
Tobias Muetze <git@tobiasmuetze.com>
2017-03-15 13:06:46 +0100

    add data files

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Sample variances of systolic blood pressure from twelve studies
+#' @description A data set containing the pooled sample variances and the
+#' respective degrees of freedom for the endpoint systolic blood pressure
+#' from twelve studies as reported in Analysis 1.1 by Glynn et al.
+#' @references Glynn LG, Murphy AW, Smith SM, Schroeder K, Fahey T.
+#' Interventions used to improve control of blood pressure in patients with hypertension.
+#' The Cochrane Library 2010; 3:CD005182.
+"varianceSPB"
+
+#' Sample variances of Hamilton Rating Scale for Depression (HAMD) score
+#' from eleven studies
+#' @description A data set containing the pooled sample variances and the
+#' respective degrees of freedom for the endpoint HAMD score
+#' from eleven studies as reported in Analysis 2.3 by Linde et al.
+#' @references Linde K, Berner MM, Kriston L.
+#' John’s wort for major depression.
+#' The Cochrane Library 2008; 4:CD000448
+"varianceHAMD"
+
